44504a2c3ca2bb3301c2c2c00e7861b06efdfdf8
Ahmed Osama <76051700+ahmedosamaft@users.noreply.github.com>
2023-08-15 00:38:01 +0300

    Update 10815 - Andy's First Dictionary.java

--- 10815 - Andy's First Dictionary.java
+++ 10815 - Andy's First Dictionary.java
@@ -1,24 +1,21 @@
-// Author: ZA7med ❤️‍🔥 
+// Author: ZA7med❤️‍🔥
+
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         // BufferedReader I = new BufferedReader(new InputStreamReader(System.in));
-        // Scanner I = new Scanner(new File("input.txt"));
-        Scanner I = new Scanner(System.in);
+         Scanner I = new Scanner(new File("input.txt"));
+        // Scanner I = new Scanner(System.in);
         BufferedWriter O = new BufferedWriter(new OutputStreamWriter(System.out));
         Set<String> ans = new TreeSet<>();
-        while (I.hasNext()){
+        while (I.hasNext()) {
             String[] line = I.nextLine().split("\\s*[^a-zA-Z]+\\s*");
-            for (String s : line) {
-                s = s.toLowerCase();
-                String cp = new String();
-                for (int i = 0; i < s.length(); i++)
-                    if(Character.isAlphabetic(s.charAt(i))) cp += s.charAt(i);
-                if(cp.length() > 0)
-                    ans.add(cp);
-            }
+            for (int i = 0; i < line.length; i++)
+                line[i] = line[i].toLowerCase();
+            ans.addAll(Arrays.asList(line));
         }
         for (String s : ans) {
             O.write(s + "\n");
@@ -26,3 +23,4 @@ public class Main {
         O.flush();
     }
 }
+
